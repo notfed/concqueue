74f167388dbb869d0e210fa6835d06f0b101d700
Jay Sullivan <jay@petio.org>
2010-08-14 19:01:56 -0400

    basic pthread usage

--- Makefile
+++ Makefile
@@ -1,5 +1,7 @@
-CC=g++
+CC=g++ -lpthread
 trypthread : trypthread.o
 	$(CC) -o trypthread trypthread.o 
 trypthread.o : trypthread.cpp
 	$(CC) -c trypthread.cpp 
+clean :
+	rm -f trypthread trypthread.o
--- trypthread.cpp
+++ trypthread.cpp
@@ -1,13 +1,15 @@
-#include <pthread>
+#include <pthread.h>
 #include <iostream>
+using namespace std;
+void* threadMain(void*)
+{
+    cout << "Hello from thread!" << endl;
+}
 int main()
 {
   pthread_t myThread;
+  pthread_attr_t myThreadAttr;
   pthread_create(&myThread, 0, threadMain, 0);
   pthread_join(myThread,0);
   return 0;
-}
-void threadMain(void*)
-{
-    cout << "Hello from thread!" << endl;
 }
