dca08f34bf9ace368bc78cd4eb7b6091c5d9484c
Jay Sullivan <jay@petio.org>
2010-08-17 15:43:28 -0400

    use 8 people for test case

--- trypthread.cpp
+++ trypthread.cpp
@@ -8,12 +8,22 @@ int main()
 {
   Room myRoom;
   DateTime now(DateTime::Now());
-  Person aaron("aaron", now+TimeSpan(12,0));
-  Person bob("bob", now+TimeSpan(10,0));
-  Person chris("chris", now+TimeSpan(15,0));
+   Person bob("bob", now+TimeSpan(2,0));
+  Person aaron("aaron", now+TimeSpan(1,0));
+     Person david("david", now+TimeSpan(4,0));
+       Person han("han", now+TimeSpan(8,0));
+    Person chris("chris", now+TimeSpan(3,0));
+     Person earl("earl", now+TimeSpan(5,0));
+      Person frank("frank", now+TimeSpan(6,0));
+      Person greg("greg", now+TimeSpan(7,0));
   myRoom.Enqueue(&aaron);
   myRoom.Enqueue(&bob);
   myRoom.Enqueue(&chris);
+  myRoom.Enqueue(&david);
+  myRoom.Enqueue(&earl);
+  myRoom.Enqueue(&frank);
+  myRoom.Enqueue(&greg);
+  myRoom.Enqueue(&han);
   myRoom.Finish();
   myRoom.Wait();
   return 0;
