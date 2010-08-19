8d6ab25600686b1850ae8b9d69ca18e45fd20cca
Jay Sullivan <jay@petio.org>
2010-08-19 00:09:59 -0400

    add void* to signature of Thread return

--- Thread.h
+++ Thread.h
@@ -6,8 +6,8 @@ class Thread
   pthread_t m_Thread;
   void Exit(void*);
 public:
-  typedef void *(*Start_routine_f)(void*);
-  typedef void *Start_routine_arg_t;
+  typedef void* (*Start_routine_f)(void*);
+  typedef void* Start_routine_arg_t;
   Thread(Start_routine_f,Start_routine_arg_t);
   virtual ~Thread() { }
   int Cancel();
