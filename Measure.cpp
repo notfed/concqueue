a317db60f5b613b6da0da7bf0506aab46e990767
Jay Sullivan <jay@petio.org>
2010-08-19 00:18:31 -0400

    update Measure operator<<

--- Measure.cpp
+++ Measure.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 Measure::~Measure()
 {
-  if(!m_Name.empty()) cerr << m_Name << " took";
-  else cerr << " Took " << (DateTime::Now() - m_StartTime);
+  if(!m_Name.empty()) cerr << m_Name << " took ";
+  else cerr << " Took ";
+  cerr << TimeSpan(DateTime::Now() - m_StartTime);
 }
