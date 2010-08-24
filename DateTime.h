7a885302c5ef321416ce26f535c8a76061f900a9
Jay Sullivan <jay@petio.org>
2010-08-24 16:03:23 -0400

    constize DateTime operators

--- DateTime.h
+++ DateTime.h
@@ -14,28 +14,28 @@ public:
   operator tm_t() const;
   operator time_t() const;
   operator timespec_t() const;
-  DateTime operator+(const TimeSpan& addend);
-  TimeSpan operator-(const DateTime& minuend);
-  bool operator< (const DateTime& right);
-  bool operator<= (const DateTime& right); 
-  bool operator>= (const DateTime& right); 
-  bool operator> (const DateTime& right); 
+  DateTime operator+(const TimeSpan& addend) const;
+  TimeSpan operator-(const DateTime& minuend) const;
+  bool operator< (const DateTime& right) const;
+  bool operator<= (const DateTime& right) const; 
+  bool operator>= (const DateTime& right) const; 
+  bool operator> (const DateTime& right) const; 
       
 };
 inline DateTime::DateTime(const tm_t& tm) { m_AbsTime = tm; }
 inline DateTime::operator tm_t() const { return m_AbsTime; }
 inline DateTime::operator time_t() const { return timegm(&m_AbsTime); }
 inline DateTime::operator timespec_t() const { timespec_t tmpTime = {static_cast<time_t>(*this),0}; return tmpTime; }
-inline TimeSpan DateTime::operator-(const DateTime& minuend) 
+inline TimeSpan DateTime::operator-(const DateTime& minuend) const
 { return TimeSpan(static_cast<int>( difftime(static_cast<time_t>(*this),
                                               static_cast<time_t>(minuend))),0); 
 }
-inline bool DateTime::operator< (const DateTime& right) 
+inline bool DateTime::operator< (const DateTime& right) const
 { return (difftime(*this,right) < 0); }
-inline bool DateTime::operator<= (const DateTime& right) 
+inline bool DateTime::operator<= (const DateTime& right) const
 { return *this==right || *this<right; }
-inline bool DateTime::operator>= (const DateTime& right) 
+inline bool DateTime::operator>= (const DateTime& right) const
 { return *this==right || *this>right; }
-inline bool DateTime::operator> (const DateTime& right) 
+inline bool DateTime::operator> (const DateTime& right) const
 { return !(*this<right); }
 #endif
