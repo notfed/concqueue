bad6fcf6c3a94a3508a2f8fcab5ac01408cac053
Jay Sullivan <jay@petio.org>
2010-08-24 15:46:29 -0400

    start fixing inline statements

--- DateTime.h
+++ DateTime.h
@@ -11,7 +11,7 @@ class DateTime
 public:
   static DateTime Now();
   inline DateTime(const tm_t& tm) { m_AbsTime = tm; }
-  inline operator tm_t&() { return m_AbsTime; }
+  inline operator tm_t() const { return m_AbsTime; }
   inline operator time_t() const { return ToTimeT(); }
   inline operator timespec_t() const { timespec_t tmpTime = {ToTimeT(),0}; return tmpTime; }
   time_t ToTimeT() const { return timegm(&m_AbsTime); }
@@ -21,14 +21,18 @@ public:
                         difftime(this->ToTimeT(),
                         minuend.ToTimeT())), 0); 
   }
-  inline friend bool operator< (const DateTime& left, const DateTime& right) 
-  { return (difftime(left,right) < 0); }
-  inline friend bool operator<= (const DateTime& left, const DateTime& right) 
-  { return left==right || left<right; }
-  inline friend bool operator>= (const DateTime& left, const DateTime& right) 
-  { return left==right || left>right; }
-  inline friend bool operator> (const DateTime& left, const DateTime& right) 
-  { return !(left<right); }
+  friend bool operator< (const DateTime& left, const DateTime& right);
+  friend bool operator<= (const DateTime& left, const DateTime& right); 
+  friend bool operator>= (const DateTime& left, const DateTime& right); 
+  friend bool operator> (const DateTime& left, const DateTime& right); 
       
 };
+inline bool operator< (const DateTime& left, const DateTime& right) 
+{ return (difftime(left,right) < 0); }
+inline bool operator<= (const DateTime& left, const DateTime& right) 
+{ return left==right || left<right; }
+inline bool operator>= (const DateTime& left, const DateTime& right) 
+{ return left==right || left>right; }
+inline bool operator> (const DateTime& left, const DateTime& right) 
+{ return !(left<right); }
 #endif
