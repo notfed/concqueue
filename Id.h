a79da47fef04f9f509344e2d179efe5146e278fa
Jay Sullivan <jay@petio.org>
2010-08-24 22:03:36 -0400

    add description for Id

--- Id.h
+++ Id.h
@@ -1,6 +1,12 @@
 #ifndef ID_H
 #define ID_H
 
+// 
+// Id.h
+//
+// The Id class can be used to create a sequence of numbers.  
+// This sequence is then used to generate unique id numbers for objects of a given class.
+// 
 template <class T>
 class Id
 {
@@ -25,5 +31,13 @@ Id<T>::operator int()
 {
   return id;
 }
+
+template <class T>
+class HasId
+{
+public:
+  const int id;
+  HasId() : id(Id<T>()) { }
+};
 
 #endif
