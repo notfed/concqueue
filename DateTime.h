e1abea4f90108f486b1e735f758b17ac6cca6071
Jay Sullivan <jay@petio.org>
2010-08-25 12:56:02 -0400

    Copy DateTime arguments for thread read safety

--- DateTime.h
+++ DateTime.h
@@ -14,8 +14,8 @@ public:
   operator tm_t() const;
   operator time_t() const;
   operator timespec_t() const;
-  DateTime operator+(const TimeSpan& addend) const;
-  TimeSpan operator-(const DateTime& minuend) const;
+  DateTime operator+(TimeSpan addend) const;
+  TimeSpan operator-(DateTime minuend) const;
   bool operator< (const DateTime& right) const;
   bool operator<= (const DateTime& right) const; 
   bool operator>= (const DateTime& right) const; 
@@ -26,7 +26,7 @@ inline DateTime::DateTime(const tm_t& tm) { m_AbsTime = tm; }
 inline DateTime::operator tm_t() const { return m_AbsTime; }
 inline DateTime::operator time_t() const { tm_t tmp = m_AbsTime; return timegm(&tmp); }
 inline DateTime::operator timespec_t() const { timespec_t tmpTime = {static_cast<time_t>(*this),0}; return tmpTime; }
-inline TimeSpan DateTime::operator-(const DateTime& minuend) const
+inline TimeSpan DateTime::operator-(DateTime minuend) const
 { return TimeSpan(static_cast<int>( difftime(static_cast<time_t>(*this),
                                               static_cast<time_t>(minuend))),0); 
 }
